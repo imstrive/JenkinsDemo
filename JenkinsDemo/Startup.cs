2144c99cfa77f6b34f1e688483cc8aa0d492efc2
imstrive <longtaosu@sina.com>
2019-03-27 22:46:59 +0800

    添加Dockerfile文件

--- JenkinsDemo/Startup.cs
+++ JenkinsDemo/Startup.cs
@@ -17,6 +17,7 @@ namespace JenkinsDemo
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
