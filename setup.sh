de76d38751d7c4cfb0a9d7c02265c15289d39d1c
imstrive <longtaosu@sina.com>
2019-03-29 16:57:44 +0800

    修改shell文件

--- setup.sh
+++ setup.sh
@@ -1,22 +1,37 @@
-echo 'start'
+# echo 'start'
 
-# variables
-echo 'variables:'
-GITHASH=`git rev-parse --short HEAD`
-IMGNAME=jenkinsdemo
-CONTAINER=jenkinsdemo
-# publish
-echo 'publish:'
-rm -rf ./publish
-dotnet publish -o ./publish
-# image
-echo 'image:'
-docker build -t $IMGNAME:$GITHASH .
-docker tag $IMGNAME:$GITHASH $IMGNAME:latest
-docker rmi -f $(docker images -q -f dangling=true)
-# container
-echo 'container:'
-docker stop $CONTAINER || true && docker rm -f $CONTAINER || true
-docker run -d --name $CONTAINER $IMGNAME
+# # variables
+# echo 'variables:'
+# GITHASH=`git rev-parse --short HEAD`
+# IMGNAME=jenkinsdemo
+# CONTAINER=jenkinsdemo
+# # publish
+# echo 'publish:'
+# rm -rf ./publish
+# dotnet publish -o ./publish
+# # image
+# echo 'image:'
+# docker build -t $IMGNAME:$GITHASH .
+# docker tag $IMGNAME:$GITHASH $IMGNAME:latest
+# docker rmi -f $(docker images -q -f dangling=true)
+# # container
+# echo 'container:'
+# docker stop $CONTAINER || true && docker rm -f $CONTAINER || true
+# docker run -d --name $CONTAINER $IMGNAME
 
-echo 'done!'
+# echo 'done!'
+
+
+
+#!/bin/sh
+image_version=`date +%Y%m%d%H%M`;
+echo $image_version;
+# cd ~/code/58HouseSearch/HouseCrawler.Core/HouseCrawler.Web;
+git pull --rebase origin master;
+docker stop jenkinsdemo;
+docker rm jenkinsdemo;
+docker build -t jenkinsdemo:$image_version .;
+docker images;
+docker run -p 8080:80 --restart=always --name jenkinsdemo -d jenkinsdemo:$image_version;
+# -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile  
+docker logs jenkinsdemo;
