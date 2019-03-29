fc13302ecc992bb07866ecd735b42553f47cd6c8
imstrive <longtaosu@sina.com>
2019-03-29 17:22:48 +0800

    sh

--- setup.sh
+++ setup.sh
@@ -28,10 +28,10 @@ image_version=`date +%Y%m%d%H%M`;
 echo $image_version;
 # cd ~/code/58HouseSearch/HouseCrawler.Core/HouseCrawler.Web;
 git pull --rebase origin master;
-sudo docker stop jenkinsdemo;
-sudo docker rm jenkinsdemo;
-sudo docker build -t jenkinsdemo:$image_version .;
+docker stop jenkinsdemo;
+docker rm jenkinsdemo;
+docker build -t jenkinsdemo:$image_version .;
 docker images;
-sudo docker run -p 8080:80 --restart=always --name jenkinsdemo -d jenkinsdemo:$image_version;
+docker run -p 8080:80 --restart=always --name jenkinsdemo -d jenkinsdemo:$image_version;
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile  
 docker logs jenkinsdemo;
